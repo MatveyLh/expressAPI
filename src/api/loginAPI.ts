40ecd7fcf669993fe772c2e1d44423e646d697ed
MatveyLh <liakh.matvei@mail.ru>
2022-07-07 14:45:46 +0300

    update swagger

--- src/api/loginAPI.ts
+++ src/api/loginAPI.ts
@@ -1,41 +1,40 @@
 import { NextFunction, Request, Response } from 'express';
 import { body } from 'express-validator';
-import { getUserByEmail } from "../utils/userActions";
-import {failResponse, successResponse} from "../utils/request";
-import { Validator } from "../utils/validator";
+import { getUserByEmail } from '../utils/userActions';
+import { failResponse, successResponse } from '../utils/request';
+import { Validator } from '../utils/validator';
 
 export class LoginAPI {
-    static routeUserLogin() {
-        return async function loginUser(req: Request, res: Response, next: NextFunction) {
-            await body('email')
-                .isEmail()
-                .run(req);
-            await body('password')
-                .not()
-                .isEmpty()
-                .withMessage('required')
-                .isLength({ min: 5 })
-                .run(req);
+  static routeUserLogin() {
+    return async function loginUser(req: Request, res: Response, next: NextFunction) {
+      await body('email')
+        .isEmail()
+        .run(req);
+      await body('password')
+        .not()
+        .isEmpty()
+        .withMessage('required')
+        .isLength({ min: 5 })
+        .run(req);
 
-            const errors = Validator.validationResult(req);
+      const errors = Validator.validationResult(req);
 
-            if (!errors.isEmpty()) {
-                return failResponse(req, res, 400, 'ERROR_PARAMS', errors.array());
-            } else {
-                return next();
-            }
-        }
+      if (!errors.isEmpty()) {
+        return failResponse(req, res, 400, 'ERROR_PARAMS', errors.array());
+      }
+      return next();
+    };
+  }
+
+  static getUser(req: Request, res: Response) {
+    const { email } = req.body;
+
+    try {
+      const userInfo = getUserByEmail(email);
+
+      return successResponse(req, res, userInfo);
+    } catch (error) {
+      return failResponse(req, res, 400, 'USER_NOT_FOUND');
     }
-
-    static getUser(req: Request, res: Response) {
-        const { email } = req.body;
-
-        try {
-            const userInfo = getUserByEmail(email);
-
-            return successResponse(req, res, userInfo);
-        } catch (error) {
-            return failResponse(req, res, 400, 'USER_NOT_FOUND');
-        }
-    }
+  }
 }
